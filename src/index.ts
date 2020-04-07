6c4095433d255e327609872fc6680a9a51735ce5
T.J. Tarazevits <tarazevits@gmail.com>
2020-04-07 00:27:42 -0700

    Implement the lookup table

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,68 @@ const port = 8080; // default port to listen
 
 app.use(bodyParser.urlencoded({extended: true}))
 
+const column1 = [
+  'integrated',
+  'total',
+  'systematized',
+  'parallel',
+  'functional',
+  'responsive',
+  'optimal',
+  'synchronized',
+  'compatible',
+  'balanced'
+];
+const column2 = [
+  'management',
+  'organizational',
+  'monitored',
+  'reciprocal',
+  'digital',
+  'logistical',
+  'transitional',
+  'incremental',
+  'third-generation',
+  'policy',
+];
+const column3 = [
+  'options',
+  'flexibility',
+  'capability',
+  'mobility',
+  'programming',
+  'concept',
+  'time-phase',
+  'projection',
+  'hardware',
+  'contingency'
+];
 
+function generateBuzzword(text: string): string {
+  let numericSeed = Number.parseInt(text, 10);
+  if (Number.isNaN(numericSeed)) {
+    numericSeed = Math.round((Math.random() * 999) + 1);
+  }
+  if (numericSeed > 999) {
+    numericSeed = 999;
+  }
+  if (numericSeed < 100 ) {
+    numericSeed = 100
+  }
+
+  const indices = numericSeed.toString().split('');
+
+  return `${column1[Number.parseInt(indices[0], 10)]} ${column2[Number.parseInt(indices[1], 10)]} ${column3[Number.parseInt(indices[2], 10)]}`
+}
 
 app.post( "/buzz", ( req, res )  => {
   console.log('buzz called');
-  console.log('request');
-  console.log(req);
   try {
     const { body } = req;
     const { text } = body;
-    res.json({ text: `current time -${Date.now()}, sent ${text}`});
+    const buzzword = generateBuzzword(text);
+    console.log(`Slack sent ${text} and we returned ${buzzword}`);
+    res.json({ text: buzzword, response_type: 'in_channel',});
   } catch (error) {
     console.log('error ', error);
     res.status(400).send(error);
