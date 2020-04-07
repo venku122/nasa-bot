2e3bfacd258e83edb030ed5da8f9bc5a13c9a192
T.J. Tarazevits <tarazevits@gmail.com>
2020-04-06 23:45:04 -0700

    Debug post body

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,25 @@
 import express from "express";
+import bodyParser from 'body-parser';
 const app = express();
 const port = 8080; // default port to listen
+
+app.use(bodyParser.urlencoded({extended: true}))
+
 
 
 app.post( "/buzz", ( req, res )  => {
   console.log('buzz called');
-  res.json({ text: `${Date.now()} current time`});
+  console.log('request');
+  console.log(req);
+  try {
+    const { body } = req;
+    const { text } = body;
+    res.json({ text: `current time -${Date.now()}, sent ${text}`});
+  } catch (error) {
+    console.log('error ', error);
+    res.status(400).send(error);
+  }
+
 } );
 
 // start the Express server
